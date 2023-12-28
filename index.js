bb338aba3b5f06343f9b3adbd8ab43fd0b50e10b
dsahddhwuihdja <sublucas.ja@gmail.com>
2023-12-28 20:55:11 +0900

    データの読み込みをreadlineにした

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const { Webhook } = require('discord-webhook-node');
+const readlineSync = require('readline-sync');
 require('date-utils');
 
-// WebhookUrlを読み込む
-const url = 'https://discord.com/api/webhooks/1189022941661380680/w8-VL95H6VO81PQzrqQKb0RU7Bay5AQCfTpuWDpgpImhYVnXFRTaBy4QmvHOjmRRkFru'
-const hook = new Webhook(url);
 
+// 時間取得
 const date = new Date();
 const currentTime = date.toFormat('HH24'+':'+'MI');
+
+// WebhookUrlを読み込む
+const url = readlineSync.question(`[`+`\x1b[33m`+`${currentTime}\x1b[37m]` + `\x1b[32m` + `(+)` + `\x1b[37mEnter Your Webhook Url>>`)
+const hook = new Webhook(url);
+
 // データの設定
 const data = {
-    username: "name",
-    content: "content"
+    username: readlineSync.question(`[`+`\x1b[33m`+`${currentTime}\x1b[37m]` + `\x1b[32m` + `(+)` + `\x1b[37mPlease Enter Webhook Name>>`),
+    content: readlineSync.question(`[`+`\x1b[33m`+`${currentTime}\x1b[37m]` + `\x1b[32m` + `(+)` + `\x1b[37mPlease Enter Message>>`)
 }
 // 送信時のメッセージ処理
 const setup = () => {
-    console.log(`[`+`\x1b[33m`+`${currentTime}\x1b[37m]` + `\x1b[32m` + `(+)` + `\x1b[37mSended` + `\x1b[36m` +` MessageContent:` + `\x1b[31m`+`${data.content}`)
+    console.log(`[`+`\x1b[33m`+`${currentTime}\x1b[37m]` + `\x1b[32m` + `(+)` + `\x1b[37mSent!` + `\x1b[36m` +` MessageContent:` + `\x1b[31m`+`${data.content}`)
 }
 // webhookに送る処理
 function send() {
