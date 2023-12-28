7482e65db1e7ddc2ee20287baf2967da416e83e8
dsahddhwuihdja <sublucas.ja@gmail.com>
2023-12-28 20:29:04 +0900

    送信時のログの処理を追加

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ const data = {
     username: "name",
     content: "content"
 }
+// 送信時のメッセージ処理
+const setup = () => {
+    console.log(`[`+`\x1b[33m`+`${currentTime}\x1b[37m]` + `\x1b[32m` + `(+)` + `\x1b[37mSended` + `\x1b[36m` +` MessageContent:` + `\x1b[31m`+`${data.content}`)
+}
 // webhookに送る処理
 function send() {
     hook.setUsername(data.username);
     hook.send(data.content);
-    console.log(`[${currentTime}]` + `\x1b[32m` + `(+)` + `\x1b[37mSended` )
 }   
 
+
+setup();
 send();
